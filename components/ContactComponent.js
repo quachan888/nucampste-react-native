0a633cb493d754422bef9a109c9f2e6315571d1d
An Quach <quachan888@gmail.com>
2022-02-25 07:43:00 +0700

    W1 assignment - Task 2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,14 +1,22 @@
 import { ScrollView, Text, View } from 'react-native';
 import React, { Component } from 'react';
+import { Card } from 'react-native-elements';
 
 export class Contact extends Component {
     static navigationOptions = {
         title: 'Contact Us'
     };
+
     render() {
         return (
             <ScrollView>
-                <Text>Hello Contact</Text>
+                <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
             </ScrollView>
         );
     }
