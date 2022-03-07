d2e044e434d396ddf33ccd582eb0559b1a239c7f
An Quach <quachan888@gmail.com>
2022-03-07 10:35:23 +0700

    Animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
@@ -42,13 +39,34 @@ function RenderItem(props) {
 }
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scaleValue: new Animated.Value(0)
+        };
+    }
+
+    animate() {
+        Animated.timing(this.state.scaleValue, {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: true
+        }).start();
+    }
+
+    componentDidMount() {
+        this.animate();
+    }
+
     static navigationOptions = {
         title: 'Home'
     };
 
     render() {
         return (
-            <ScrollView>
+            <Animated.ScrollView
+                style={{ transform: [{ scale: this.state.scaleValue }] }}
+            >
                 <RenderItem
                     item={
                         this.props.campsites.campsites.filter(
@@ -76,7 +94,7 @@ class Home extends Component {
                     isLoading={this.props.partners.isLoading}
                     errMess={this.props.partners.errMess}
                 />
-            </ScrollView>
+            </Animated.ScrollView>
         );
     }
 }
