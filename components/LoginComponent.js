c30cf42ee7bbcd4ed66062c109af371bdfa2307a
An Quach <quachan888@gmail.com>
2022-03-17 10:01:12 +0700

    W4 - Task 2 - ImagePicker Camera Roll

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -146,7 +146,6 @@ class RegisterTab extends Component {
             if (!capturedImage.cancelled) {
                 console.log('getImageFromCamera', capturedImage);
                 this.processImage(capturedImage.uri);
-                // this.setState({ imageUrl: capturedImage.uri });
             }
         }
     };
@@ -159,6 +158,21 @@ class RegisterTab extends Component {
         );
         console.log('processImage', processedImage);
         this.setState({ imageUrl: processedImage.uri });
+    };
+
+    getImageFromGallery = async () => {
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log('capturedImage', capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
     };
 
     handleRegister() {
@@ -195,6 +209,7 @@ class RegisterTab extends Component {
                         style={styles.image}
                     />
                     <Button title="Camera" onPress={this.getImageFromCamera} />
+                    <Button title="Galery" onPress={this.getImageFromGallery} />
                 </View>
                 <View style={styles.container}>
                     <Input
